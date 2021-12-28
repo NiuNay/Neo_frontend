c4e45cc8f5863a8fdd7b918400d775d283880540
jurkovicova <mich.jurkovicova@gmail.com>
2021-12-28 15:22:17 +0000

    finished calibration, comment and prick reading page layout
    add all raw components for the calibration, comment and prick page with some rough layout

--- src/components/Calibration.jsx
+++ src/components/Calibration.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import neologo from "./NeoLogo.png";
+import styled from 'styled-components'
+
 
 function Calibration() {
     return (
@@ -7,9 +9,93 @@ function Calibration() {
             <center>
                 <img src={neologo} height={55} width={112} style={{ margin: '30px' }}/>
             </center>
-            <h1 className = "text-center" style={{ color: '#565656', fontFamily: 'ruluko', fontWeight: "bold", fontSize: "40px"}}>Calibration</h1>
+            <h1 className = "text-center" style={{ color: '#565656', fontFamily: 'ruluko', fontWeight: "bold", fontSize: "40px"}}>Calibration and Settings</h1>
+            <h2 style={text1}>Linear Function Callibration</h2>
+            
+            <form style={form1}> 
+                <label>Input slope: 
+                    <input type="text" />
+                </label>
+            </form>
+            <form style={form2}> 
+                <label>Input offset:
+                    <input type="text" />
+                </label>
+            </form>
+            <h2 style={text2}>Sweat reading settings</h2>
+            <form style={form3}> 
+                <label>Time delay:
+                    <input type="text"  />
+                </label>
+            </form>
+            <div className= "button-grid-2">
+                <a href="./menu">
+                    <BackButton> Back </BackButton>
+                </a>
+                <a href="./menu">
+                    <BackButton> Save </BackButton>
+                </a>
+            </div>
         </div>
     )
 }
 
 export default Calibration
+
+const text1 = {
+    position: 'relative',
+    top: "25px",
+    left: "100px",
+    fontSize: 25,
+    ontFamily: 'ruluko', 
+    fontWeight: "bold",
+    color: '#565656'
+}
+
+const text2 = {
+    position: 'relative',
+    top: "150px",
+    left: "100px",
+    fontSize: 25,
+    ontFamily: 'ruluko', 
+    fontWeight: "bold",
+    color: '#565656'
+}
+
+const form1 = {
+    position: 'absolute',
+    top: "30%",
+    left: "40%",
+    fontSize: 20,
+    ontFamily: 'ruluko', 
+    color: '#565656',
+  
+}
+
+const form2 = {
+    position: 'absolute',
+    top: "40%",
+    left: "40%",
+    fontSize: 20,
+    ontFamily: 'ruluko', 
+    color: '#565656',
+}
+
+const form3 = {
+    position: 'absolute',
+    top: "60%",
+    left: "40%",
+    fontSize: 20,
+    ontFamily: 'ruluko', 
+    color: '#565656',
+}
+
+const BackButton = styled.button`
+  background-color: #E9E9E9;
+  color: #515050;
+  font-size: 20px;
+  font-family: ruluko;
+  border-radius: 5px;
+  cursor: pointer;
+  width: 100%
+`;
--- src/components/PrickReading.jsx
+++ src/components/PrickReading.jsx
@@ -1,15 +1,93 @@
 import React from 'react';
 import neologo from "./NeoLogo.png";
+import styled from 'styled-components'
+import TimeInput from 'react-input-time';
 
 function PrickReading() {
+
+    var today = new Date(),
+    date = new String(),
+    time = today.getHours() + ':' + today.getMinutes()
+    date = today.getDate() + '/' + (today.getMonth()+1) + '/' +  today.getFullYear()
+
     return (
         <div>
             <center>
                 <img src={neologo} height={55} width={112} style={{ margin: '30px' }}/>
             </center>
-            <h1 className = "text-center" style={{ color: '#565656', fontFamily: 'ruluko', fontWeight: "bold", fontSize: "40px"}}>Prick Reading</h1>
+            <h1 className = "text-center" style={{ color: '#565656', fontFamily: 'ruluko', fontWeight: "bold", fontSize: "40px"}}>Comment</h1>
+        <div className= "button-grid-2">
+            <a href="./menu">
+                <BackButton> Back </BackButton>
+            </a>
+            <a href="./menu">
+                <BackButton> Save </BackButton>
+            </a>
+
         </div>
+       
+        <div className="grid-columns">
+            <text style={text1}> Input prick readings </text>
+            <input type="text"></input> 
+            <text style={text1}> Input date </text>
+            <input
+                className="input-time"
+                defaultValue={date}
+                onChange={(event) => {}}>            
+            </input>
+            <text style={text1}> Input time </text>  
+            <TimeInput className="input-time" initialTime= {time} onChange={(event) => {}} />
+        </div>
+     
+         </div>
+
+
     )
 }
 
 export default PrickReading
+
+  
+const form1 = {
+    position: 'absolute',
+    top: "40%",
+    left: "40%",
+    fontSize: 20,
+    ontFamily: 'ruluko', 
+    color: '#565656',
+    
+    }
+    
+    const form2 = {
+    position: 'absolute',
+    top: "50%",
+    left: "40%",
+    fontSize: 20,
+    ontFamily: 'ruluko', 
+    color: '#565656',
+    }
+    
+    const form3 = {
+    position: 'absolute',
+    top: "60%",
+    left: "40%",
+    fontSize: 20,
+    ontFamily: 'ruluko', 
+    color: '#565656',
+    }
+    
+    const BackButton = styled.button`
+    background-color: #E9E9E9;
+    color: #515050;
+    font-size: 20px;
+    font-family: ruluko;
+    border-radius: 5px;
+    cursor: pointer;
+    width: 100%
+    `;
+
+    const text1 = {
+        fontSize: 20,
+        ontFamily: 'ruluko', 
+        color: '#565656',
+    }
