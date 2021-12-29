85980c19a27f50bac896e589aa17b086deca4f0b
jurkovicova <mich.jurkovicova@gmail.com>
2021-12-29 01:31:57 +0000

    add input note functionality

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,49 +1,108 @@
-import React from 'react';
+import React, { Component } from 'react';
 import neologo from "./NeoLogo.png";
 import styled from 'styled-components'
 import TimeInput from 'react-input-time';
+import UserService from '../services/UserService';
 
-function Comment() {
 
-    var today = new Date(),
-    date = new String(),
-    time = today.getHours() + ':' + today.getMinutes()
-    date = today.getDate() + '/' + (today.getMonth()+1) + '/' +  today.getFullYear()
 
+var today = new Date(),
+defDate = new String(),
+deftime = new String(),
+defDate=today.getDate() + '/' + (today.getMonth()+1) + '/' +  today.getFullYear()
+deftime =today.getHours() + ':' + today.getMinutes()
+const id = localStorage.getItem("selectedPatient");
+
+class Comment extends Component {
+
+
+
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            id: id,
+            date: defDate,
+            time: deftime,
+	        note:''
+        }
+        this.changeCommentHandler = this.changeCommentHandler.bind(this);
+        this.changeDateHandler = this.changeDateHandler.bind(this);
+        this.saveNote = this.saveNote.bind(this);
+    }
+
+    componentDidMount(){
+
+            UserService.getEmployeeById(this.state.id).then( (res) =>{
+                let employee = res.data;
+                this.setState({note: employee.note,
+                    time_instant: employee.time_instant,
+                    
+                });
+            });
+               
+    }
+
+
+    saveNote = (e) => {
+        e.preventDefault();
+        let employee = {note: this.state.note, time_instant: this.state.date + ' ' + this.state.time};
+        console.log('employee => ' + JSON.stringify(employee));
+        UserService.addNote(employee,this.state.id).then(res =>{
+            this.props.history.push('/menu')
+        });
+        
+    }
+    
+    changeCommentHandler= (event) => {
+
+        this.setState({note: event.target.value});
+    }
+
+    changeDateHandler= (event) => {
+
+        this.setState({date: event.target.value});
+    }
+
+    changeTimeHandler= (event) => {
+
+        this.setState({time: event.target.value});
+    }
+
+render(){
     return (
         <div>
+
             <center>
                 <img src={neologo} height={55} width={112} style={{ margin: '30px' }}/>
             </center>
             <h1 className = "text-center" style={{ color: '#565656', fontFamily: 'ruluko', fontWeight: "bold", fontSize: "40px"}}>Comment</h1>
+       
+         <div className="grid-columns">
+            <text style={text1}> Input comment </text>
+            <input placeholder="type note..." name="note" className="form-control" value={this.state.note} onChange={this.changeCommentHandler}/>
+            <text style={text1}> Input date </text>
+            <input name="time_instant" className="form-control" value={this.state.date} onChange={this.changeDateHandler}/>
+            <text style={text1}> Input time </text>
+            <TimeInput defaultValue={deftime} className="form-control" value= {this.state.time} onChange={this.changeTimeHandler} /> 
+        </div> 
+     
         <div className= "button-grid-2">
             <a href="./menu">
-                <BackButton> Back </BackButton>
+                <BackButton> Back to menu </BackButton>
             </a>
             <a href="./menu">
-                <BackButton> Save </BackButton>
+                <BackButton onClick={this.saveNote}>Save</BackButton>
             </a>
 
         </div>
-       
-        <div className="grid-columns">
-            <text style={text1}> Input comment </text>
-            <input type="text"></input> 
-            <text style={text1}> Input date </text>
-            <input
-                className="input-time"
-                defaultValue={date}
-                onChange={(event) => {}}>            
-            </input>
-            <text style={text1}> Input time </text>  
-            <TimeInput className="input-time" initialTime= {time} onChange={(event) => {}} />
-        </div>
+    
      
          </div>
 
 
     )
-}
+}}
 
 export default Comment
 
--- src/services/UserService.js
+++ src/services/UserService.js
@@ -1,7 +1,9 @@
 import axios from 'axios'
 
 const patient_list_url = 'http://localhost:8080';
+const EMPLOYEE_API_BASE_URL = 'http://localhost:8080';
 const patient_data_url = 'http://localhost:8080/124790';
+
 
 class UserService {
 
@@ -12,6 +14,31 @@ class UserService {
     getData(){
         return axios.get(patient_data_url);
     }
+
+
+getEmployees(){
+    return axios.get(EMPLOYEE_API_BASE_URL);
+}
+
+addPrickData(selectedPatient){
+    return axios.post(EMPLOYEE_API_BASE_URL +'/'+ selectedPatient);
+}
+
+addNote(employee,id){
+    return axios.post(EMPLOYEE_API_BASE_URL +'/'+ id + '/addNote', employee) ;
+}
+
+getEmployeeById(employeeId){
+    return axios.get(EMPLOYEE_API_BASE_URL + '/' + employeeId);
+}
+
+updateEmployee(employee, employeeId){
+    return axios.put(EMPLOYEE_API_BASE_URL + '/' + employeeId, employee);
+}
+
+deleteEmployee(employeeId){
+    return axios.delete(EMPLOYEE_API_BASE_URL + '/' + employeeId);
+}
 }
 
 export default new UserService();
