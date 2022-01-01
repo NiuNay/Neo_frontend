4f49112d99e9f8a2f092cd7bfc686f84cd76a1b7
niunay <agp119@ic.ac.uk>
2022-01-01 19:01:41 +0100

    Glucose levels graphing works: fully connected to backend and plotting correct values

--- src/components/GlucoseLevels.jsx
+++ src/components/GlucoseLevels.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import neologo from "./NeoLogo.png";
+import styles from "./styles.css"
 import CanvasJSReact from '../lib/canvasjs.react';
 import styled from "styled-components";
 import UserService from "../services/UserService";
@@ -13,9 +14,12 @@ class GlucoseLevels extends React.Component {
         super(props)
         this.state = {
             id: id,
-            time_data:[],
-            current_data: [],
-            length: 0,
+            sweat_time_data:[],
+            sweat_glucose_data: [],
+            prick_time_data: [],
+            prick_glucose_data: [],
+            sweat_data_length: 0,
+            prick_data_length: 0,
             title: "Select Patient ID"
         }
     }
@@ -23,8 +27,8 @@ class GlucoseLevels extends React.Component {
     componentDidMount(){
         UserService.getData(this.state.id)
             .then((response) => {
-                this.setState({ time_data: response.data[0], current_data: response.data[1]})
-                this.setState({len: this.state.time_data.length})
+                this.setState({ sweat_time_data: response.data[0], sweat_glucose_data: response.data[1], prick_time_data: response.data[2], prick_glucose_data: response.data[3]})
+                this.setState({sweat_data_length: this.state.sweat_time_data.length, prick_data_length: this.state.prick_time_data.length})
             })
             .catch(() => {                          // checks data was retrieved
                 alert("Error retrieving patient data");
@@ -46,15 +50,17 @@ class GlucoseLevels extends React.Component {
           margin-right:40%;
         `;
 
-        const timeData = this.state.time_data
-        const currentData = this.state.current_data
-        var dataPlot = [];
-        var t = new Date();
-        for (let i = 0; i < this.state.len; i++) {
-            t = this.state.time_data[i];
-            dataPlot.push({x: t, y: this.state.current_data[i]})
+        var sweat_data = [];
+        for (let i = 0; i < this.state.sweat_data_length; i++) {
+            var t = new Date(this.state.sweat_time_data[i]);
+            sweat_data.push({x: t, y: this.state.sweat_glucose_data[i]})
         }
-        // RIGHT FORMAT BUT CONVERT TO DATE TIME - didn't work yet...
+
+        var prick_data = [];
+        for (let i = 0; i < this.state.prick_data_length; i++) {
+            var t = new Date(this.state.prick_time_data[i]);
+            prick_data.push({x: t, y: this.state.prick_glucose_data[i]})
+        }
 
         const options = {
             animationEnabled: true,
@@ -78,32 +84,19 @@ class GlucoseLevels extends React.Component {
                 name: "Sweat Data",
                 showInLegend: true,
                 toolTipContent: "{x}: {y}mmol/L",
-                dataPoints: []
-                //     [//array
-                //     { x: new Date(2012, 1, 1), y: 26},
-                //     { x: new Date(2012, 1, 3), y: 38},
-                //     { x: new Date(2012, 1, 5), y: 43},
-                //     { x: new Date(2012, 1, 7), y: 29},
-                //     { x: new Date(2012, 1, 11), y: 41},
-                //     { x: new Date(2012, 1, 13), y: 54},
-                //     { x: new Date(2012, 1, 20), y: 66},
-                //     { x: new Date(2012, 1, 21), y: 60},
-                //     { x: new Date(2012, 1, 25), y: 53},
-                //     { x: new Date(2012, 1, 27), y: 60}
-                // ]
+                dataPoints: sweat_data
             }, {
                 type: "scatter",
                 name: "Prick Data",
                 showInLegend: true,
                 toolTipContent: "{x}: {y}mmol/L",
-                dataPoints: dataPlot
+                dataPoints: prick_data
                 }
             ]
         }
 
         return (
             <div>
-                {timeData}
                 <center>
                     <img src={neologo} height={55} width={112} style={{ margin: '30px' }}/>
                 </center>
