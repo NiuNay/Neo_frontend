239d1e1b54549cf95a59dbff80f65699c3f52a12
niunay <agp119@ic.ac.uk>
2022-01-07 17:14:58 +0100

    Deleted useless files

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import './index.css';
-import 'typeface-roboto'
-import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import {
@@ -32,8 +30,3 @@ ReactDOM.render(
 
     document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
